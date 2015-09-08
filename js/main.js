1a57c00eeaaf60b7657889466f874f0ad4b11075
Philippe Simpson <philippe@ultimatemanager.com>
2015-09-08 14:58:34 +0200

    summery, nav, styling

--- js/main.js
+++ js/main.js
@@ -2,21 +2,19 @@
  * Created by Philippe Simpson on 06-09-2015.
  */
 
-// Model class for each card item
+// Model class for each card item:
 var CardModel = Backbone.Model.extend({
     defaults: {
-        id: null,
-        name: null,
-        occupation: null
+        url: "https://www.trustpilot.com/review/www.trustpilot.com/55ee48b00000ff0009064ac0" // specific review url not provided, so we add one.
     }
 });
 
-// Collection class for the card item list
+// Collection class for the card item list:
 var CardCollection = Backbone.Collection.extend({
     model: CardModel
 });
 
-// View class for displaying each card item
+// View class for displaying each card item:
 var CardItemView = Backbone.View.extend({
     className: 'card',
     template: _.template($('#review-card-template').html()),
@@ -40,18 +38,63 @@ var CardItemView = Backbone.View.extend({
     }
 });
 
-// View class for displaying Trustscore
+// View class for displaying Trustscore:
 var SummeryView = Backbone.View.extend({
-    className: 'summery',
     template: _.template($('#summery-template').html()),
 
     initialize: function() {
+        // lacking real review summery data we construct some out of the provided sample reviews:
+        var sum = this.collection.pluck("starRating").reduce(function(previousValue, currentValue, index, array) {
+            return parseInt(previousValue) + parseInt(currentValue);
+        });
+        var avg = sum / this.collection.length;
+        var color;
 
+        switch(avg) {
+            case 1:
+                color = "#e22027";
+                break;
+            case 2:
+                color = "#f47324;";
+                break;
+            case 3:
+                color = "#f8cc18";
+                break;
+            case 4:
+                color = "#73b143";
+                break;
+            case 5:
+                color = "#e22027";
+                break;
+            default:
+                color = "#007f4e";
+        }
+
+        this.model.set({
+            average: avg,
+            base: this.collection.length,
+            color: color
+        });
     },
 
     render: function() {
         this.$el.html(this.template(this.model.attributes));
+
+        this.animateStars(0);
+
         return this;
+    },
+
+    animateStars: function(index){
+        var starEl = this.$el.find(".star").eq(index);
+        _.delay(this.showStar, 300, starEl, index, this);
+    },
+
+    showStar: function(el, index, that){
+        // this function is out of scope when envoked: use passed 'that'.
+
+        el.css("background-color", that.model.get('color'));
+        if(index < that.model.get('average') - 1) that.animateStars(index + 1);
     },
 
     events: {
@@ -87,6 +130,7 @@ function startWidget(data){
 
     showCard(cardsCollection.at(0));
 
+    $('.nav a').click(this.nextCard);
     //showCard(cardsCollection.at(count));
     //var timer = setInterval(nextCard, 2000);
 }
